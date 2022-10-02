fdbc5823f827b692333a55add0fd9eee1d6a75ae
Linsong Liu <liu.linsong@columbia.edu>
2022-10-02 22:12:23 +0000

    finished

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,12 @@
 class MoviesController < ApplicationController
 
   def show
-    logger.info "****Print on show****"
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def api(params)
-    logger.info "****Print on api****"
-    logger.info params['ratings']
-
     if params['ratings'] == nil and params['sorting'] == nil
       return
     end
@@ -25,8 +21,6 @@ class MoviesController < ApplicationController
   end
 
   def index
-    logger.info "****Print on index****"
-    logger.info params
     @all_ratings = ['G','PG','PG-13','R']
 
     if params != {"controller"=>"movies", "action"=>"index"}
@@ -51,7 +45,6 @@ class MoviesController < ApplicationController
       if @sorting != nil
         new_params['sorting'] = @sorting
       end
-      logger.info new_params
 
       redirect_to movies_path(new_params)
       return
@@ -63,24 +56,20 @@ class MoviesController < ApplicationController
   end
 
   def new
-    logger.info "****Print on new****"
     # default: render 'new' template
   end
 
   def create
-    logger.info "****Print on create****"
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
   def edit
-    logger.info "****Print on edit****"
     @movie = Movie.find params[:id]
   end
 
   def update
-    logger.info "****Print on update****"
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
@@ -88,7 +77,6 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    logger.info "****Print on destroy****"
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
@@ -99,7 +87,6 @@ class MoviesController < ApplicationController
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
   def movie_params
-    logger.info "****Print on movie_params****"
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 end
